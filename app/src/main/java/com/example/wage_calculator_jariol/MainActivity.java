934d9c08096343b2dbc2adf2945967c5da418eee
AstinTabora <astinjohndtabora2@gmail.com>
2024-05-28 12:47:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wage_calculator_jariol/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.wage_calculator_jariol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.wage_calculator_jariol.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView result;
+    EditText num1;
+    EditText num2;
+    Button calculate;
+    Button clear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        num1 = findViewById(R.id.txtNum1);
+        num2 = findViewById(R.id.txtNum2);
+        result = findViewById(R.id.txtResult);
+        calculate = findViewById(R.id.btnCalculate);
+        clear = findViewById(R.id.btnClear);
+
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double number1 = Double.parseDouble(num1.getText().toString());
+                double number2 = Double.parseDouble(num2.getText().toString());
+                double product = number1 * number2;
+                result.setText(String.valueOf("₱" + product));
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num1.setText(" ");
+                num2.setText(" ");
+                result.setText("00.00");
+            }
+        });
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WAGE_CALCULATOR_JARIOL"
+include(":app")
+ 
